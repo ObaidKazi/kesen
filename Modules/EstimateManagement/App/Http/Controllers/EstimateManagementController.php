db29ab43dd8e23e61aeb33d679c12ad42dc7d53c
Obaid Kazi <obaid@bravonet.my>
2024-05-29 05:42:07 +0530

    quick fix

--- Modules/EstimateManagement/App/Http/Controllers/EstimateManagementController.php
+++ Modules/EstimateManagement/App/Http/Controllers/EstimateManagementController.php
@@ -77,7 +77,7 @@ class EstimateManagementController extends Controller
             'layout_charges_second.*' => 'required|string|max:255',
             'lang.*' => 'required|string|max:255',
         ]);
-        
+
         $estimate = new Estimates();
         $estimate->estimate_no = $request->estimate_no;
         $estimate->metrix = $request->metrix;
@@ -87,25 +87,26 @@ class EstimateManagementController extends Controller
         $estimate->amount = $request->amount;
         $estimate->currency = $request->currency;
         $estimate->status = $request->status;
-        $estimate->discount = $request->discount??0;
+        $estimate->discount = $request->discount ?? 0;
         $estimate->created_by = Auth()->user()->id;
         $estimate->updated_by = Auth()->user()->id;
         $estimate->save();
-        
-        foreach ($request['document_name'] as $index => $document_name) {
-            EstimatesDetails::create([
-                'estimate_id' => $estimate->id,
-                'document_name' => $document_name,
-                'type' => $request['type'][$index],
-                'unit' => $request['unit'][$index],
-                'rate' => $request['rate'][$index],
-                'verification' => $request['verification'][$index],
-                'verification_2' => $request['verification_2'][$index],
-                'back_translation' => $request['back_translation'][$index],
-                'layout_charges' => $request['layout_charges'][$index],
-                'layout_charges_2' => $request['layout_charges_second'][$index],
-                'lang' => $request['lang'][$index]
-            ]);
+        if ($request['document_name'] != null) {
+            foreach ($request['document_name'] as $index => $document_name) {
+                EstimatesDetails::create([
+                    'estimate_id' => $estimate->id,
+                    'document_name' => $document_name,
+                    'type' => $request['type'][$index],
+                    'unit' => $request['unit'][$index],
+                    'rate' => $request['rate'][$index],
+                    'verification' => $request['verification'][$index],
+                    'verification_2' => $request['verification_2'][$index],
+                    'back_translation' => $request['back_translation'][$index],
+                    'layout_charges' => $request['layout_charges'][$index],
+                    'layout_charges_2' => $request['layout_charges_second'][$index],
+                    'lang' => $request['lang'][$index]
+                ]);
+            }
         }
         Session::flash('message', 'Estimate created successfully');
         return redirect('/estimate-management');
@@ -117,7 +118,7 @@ class EstimateManagementController extends Controller
     public function show($id)
     {
         $estimate = Estimates::where('id', $id)->with('client', 'client_person')->first();
-        $estimate_details=EstimatesDetails::where('estimate_id',$id)->get();
+        $estimate_details = EstimatesDetails::where('estimate_id', $id)->get();
         return view('estimatemanagement::show', compact('estimate', 'estimate_details'));
     }
 
@@ -128,8 +129,8 @@ class EstimateManagementController extends Controller
     {
         $estimate = Estimates::find($id);
         $contact_persons = ContactPerson::where('client_id', $estimate->client_id)->get();
-        $estimate_details=EstimatesDetails::where('estimate_id',$id)->get();
-        return view('estimatemanagement::edit', compact('estimate', 'contact_persons','estimate_details'));
+        $estimate_details = EstimatesDetails::where('estimate_id', $id)->get();
+        return view('estimatemanagement::edit', compact('estimate', 'contact_persons', 'estimate_details'));
     }
 
     /**
@@ -144,7 +145,7 @@ class EstimateManagementController extends Controller
             'client_contact_person_id' => 'required',
             'headline' => 'nullable',
             'amount' => 'required',
-            'discount'=>'required',
+            'discount' => 'required',
             'currency' => 'required',
             'status' => 'required|in:1,0,2',
             'document_name.*' => 'required|string|max:255',
@@ -165,15 +166,16 @@ class EstimateManagementController extends Controller
         $estimate->client_contact_person_id = $request->client_contact_person_id;
         $estimate->headline = $request->headline;
         $estimate->amount = $request->amount;
-        $estimate->discount = $request->discount??0;
+        $estimate->discount = $request->discount ?? 0;
         $estimate->currency = $request->currency;
         $estimate->status = $request->status;
         $estimate->updated_by = Auth()->user()->id;
         $estimate->save();
+        if ($request['document_name'] != null) {
         foreach ($request['document_name'] as $index => $document_name) {
             EstimatesDetails::updateOrCreate([
-                'id' => isset($request['id'][$index])?$request['id'][$index]:null
-            ],[
+                'id' => isset($request['id'][$index]) ? $request['id'][$index] : null
+            ], [
                 'estimate_id' => $estimate->id,
                 'document_name' => $document_name,
                 'type' => $request['type'][$index],
@@ -187,6 +189,7 @@ class EstimateManagementController extends Controller
                 'lang' => $request['lang'][$index]
             ]);
         }
+    }
         Session::flash('message', 'Estimate updated successfully');
         return redirect('/estimate-management');
     }
@@ -203,28 +206,28 @@ class EstimateManagementController extends Controller
     public function deleteDetail($id)
     {
         $detail = EstimatesDetails::findOrFail($id);
-        if($detail==null){
-            return response()->json(['success' => 'Detail not found'],403);
+        if ($detail == null) {
+            return response()->json(['success' => 'Detail not found'], 403);
         }
         $detail->delete();
         return response()->json(['success' => 'Detail deleted successfully']);
     }
 
 
-    public function getEstimateDetails($id){
+    public function getEstimateDetails($id)
+    {
         $html = '<option value="">Select Estimate Document</option>';
-        if($id==null || $id==''){
+        if ($id == null || $id == '') {
             return response()->json(['html' => $html]);
         }
-        $job_register=JobRegister::where('estimate_id',$id)->pluck('estimate_document_id');
-        $estimate_details=EstimatesDetails::where('estimate_id',$id)->whereNotIn('document_name',$job_register)->distinct()->pluck('document_name');
-        if(count($estimate_details)>0){
+        $job_register = JobRegister::where('estimate_id', $id)->pluck('estimate_document_id');
+        $estimate_details = EstimatesDetails::where('estimate_id', $id)->whereNotIn('document_name', $job_register)->distinct()->pluck('document_name');
+        if (count($estimate_details) > 0) {
             $html = '<option value="">Select Estimate Document</option>';
             foreach ($estimate_details as $document_name) {
-                $html .= '<option value='.$document_name.'>'.$document_name.'</option>';
+                $html .= '<option value=' . $document_name . '>' . $document_name . '</option>';
             }
         }
         return response()->json(['html' => $html]);
-        
     }
 }
