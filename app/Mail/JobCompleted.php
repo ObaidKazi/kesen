5392547465a7a24ea72fb1a25fba25a1128de4d9
Haris Shaikh <compilerharris@gmail.com>
2024-07-16 07:18:32 +0530

    Changes from Haris

--- app/Mail/JobCompleted.php
+++ app/Mail/JobCompleted.php
@@ -33,6 +33,6 @@ class JobCompleted extends Mailable
     {
         return $this->subject('Job Completed')
                     ->view('jobcardmanagement::emails.jobCompleted')->with('jobDetails', $this->jobDetails)
-                    ->attach(public_path('pdf/feedback_form.pdf'));
+                    ->attach(public_path('pdf/CUSTOMER-SATISFACTION-SURVEY.pdf'));
     }
 }
