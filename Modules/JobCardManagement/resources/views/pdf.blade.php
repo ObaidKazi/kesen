c1e8b2d7e563d53cf83e343fd714853e8675998d
Haris Shaikh <compilerharris@gmail.com>
2024-07-18 11:43:18 +0530

    Changes from Haris

--- Modules/JobCardManagement/resources/views/pdf.blade.php
+++ Modules/JobCardManagement/resources/views/pdf.blade.php
@@ -110,13 +110,13 @@
     <div class="container">
         <div class="header">
             @if ($job->estimate->client->client_metric->code == 'KCP')
-                <img src="{{ public_path('img/kesen-communication.jpeg') }}" alt="Kesen Communication" width="100%">
+                <img src="{{ public_path('img/kesen-communication.jpeg') }}" alt="Iceberg Image" width="100%">
             @elseif ($job->estimate->client->client_metric->code == 'KLB')
-                <img src="{{ public_path('img/kesen-language-buea.jpeg') }}" alt="Kesen Language Bureau" width="100%">
+                <img src="{{ public_path('img/kesen-language-buea.jpeg') }}" alt="Iceberg Image" width="100%">
             @elseif ($job->estimate->client->client_metric->code == 'LGS')
-                <img src="{{ public_path('img/kesen-linguist-Servi-llp.jpeg') }}" alt="Kesen Linguist Servi LLP" width="100%">
+                <img src="{{ public_path('img/kesen-linguist-system.jpeg') }}" alt="Iceberg Image" width="100%">
             @else
-                <img src="{{ public_path('img/kesen-linguist-system.jpeg') }}" alt="Kesen Linguist System" width="100%">
+                <img src="{{ public_path('img/kesen-linguist-Servi-llp.jpeg') }}" alt="Iceberg Image" width="100%">
             @endif
             <br>
             <br>
